da1fc8af9cd3ed4827783244b90de946b6fc1672
Jaco Labuschagne <zodimo@gmail.com>
2025-09-12 08:23:32 +0200

    Add logging capabilities to WriteCloser in middleware
    - Introduced a new `logWriteCloser` type that wraps `cbio.WriteCloser` to add logging functionality for write and close operations.
    - Implemented `NewLogWriteCloser` to create instances of `logWriteCloser` with a specified prefix for log messages.
    - Enhanced the `Write` and `Close` methods to log the number of bytes written and the closing action, respectively.
    - Updated the `NewLoggingMiddleware` function to utilize `logWriteCloser`, improving visibility into connection handling in the middleware.

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -7,12 +7,33 @@ import (
 	"github.com/zodimo/go-netkit/cbio"
 )
 
+var _ cbio.WriteCloser = (*logWriteCloser)(nil)
+
+type logWriteCloser struct {
+	cbio.WriteCloser
+	prefix string
+}
+
+func NewLogWriteCloser(writeCloser cbio.WriteCloser, prefix string) cbio.WriteCloser {
+	return &logWriteCloser{WriteCloser: writeCloser, prefix: prefix}
+}
+
+func (l *logWriteCloser) Write(p []byte, handler cbio.WriterHandler, options ...cbio.WriterOption) (cbio.CbContext, error) {
+	fmt.Printf("[LOGGER %s] Writing %d bytes\n", l.prefix, len(p))
+	return l.WriteCloser.Write(p, handler, options...)
+}
+
+func (l *logWriteCloser) Close(options ...cbio.CloserOption) error {
+	fmt.Printf("[LOGGER %s] Closing\n", l.prefix)
+	return l.WriteCloser.Close(options...)
+}
+
 func NewLoggingMiddleware(prefix string) func(handler netkit.TransportHandler) netkit.TransportHandler {
 	return func(handler netkit.TransportHandler) netkit.TransportHandler {
 		return netkit.NewTransportHandler(
 			func(conn cbio.WriteCloser) {
 				fmt.Printf("[LOGGER %s] Connection opened \n", prefix)
-				handler.OnOpen(conn)
+				handler.OnOpen(NewLogWriteCloser(conn, prefix))
 			},
 			func(message []byte) {
 				fmt.Printf("[LOGGER %s] Message received: %s\n", prefix, string(message))
